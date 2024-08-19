cc0397d402eae12160941692cab31415aac93659
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2024-08-19 16:12:36 +0300

    init commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+
+export default App = () => {
+  return (
+    <View>
+      <Link href={'/home'}>home</Link>
+    </View>
+  );
+};
